8a69b6e743fa20a3a28757acc6a95c05a3e0d174
tapeterman <tapetermann@outlook.com.br>
2018-04-02 13:10:07 -0300

    remover pastas inutilizadas

--- orion/op/restrito.php
+++
@@ -1,7 +0,0 @@
-<?php
-	if (!isset($_SESSION)) session_start();
-		$nivelpermissao = 1;
-	if (!isset($_SESSION['Almope']) or ($_SESSION['Permissao'] < $nivelpermissao)) {
-		session_destroy();
-		header("Location: ../index.php"); exit;
-	}
